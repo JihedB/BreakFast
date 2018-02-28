bf3e9a0b8408f019c9d5a41f99bb609d9e159256
Jihed <L103@L103>
2018-02-28 14:46:13 +0100

    First Push

---
+++ J.Models/Context/BreakDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace J.Models.Context
+{
+    public partial class BreakDBContext : DbContext
+    {
+        public BreakDBContext() : base("name=BreakDataBase")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //  public virtual DbSet<Menu> menus { get; set; }
+    }
+}
